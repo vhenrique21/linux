76e498825c9b5be92e5d84b1ffc0be384b8756a9
Eric Pires <ericpires9@gmail.com>
2017-05-20 14:20:47 -0300

    Added stack functionality

--- sys_stack/sys_stack.c
+++ sys_stack/sys_stack.c
@@ -1,11 +1,40 @@
 #include <linux/linkage.h>
+#include <linux/list.h>
+#include <linux/slab.h>
+
+static LIST_HEAD(stack);
+
+struct stack_value {
+    int value;
+    struct list_head stack;
+};
 
 asmlinkage long sys_stack_push(int value)
 {
-    return value + 10;
+    struct stack_value* new_stack_value;
+    
+    new_stack_value = kmalloc(sizeof(*new_stack_value), GFP_KERNEL);
+    new_stack_value->value = value;
+    INIT_LIST_HEAD(&new_stack_value->stack);
+
+    list_add(&new_stack_value->stack, &stack);
+    return 0;
 }
 
 asmlinkage long sys_stack_pop(void)
 {
-    return 0;
+    int value;
+    struct stack_value* top_stack_value;
+
+    if (list_empty(&stack)) {
+        /* TODO: Alertar alguem */
+        return -1;
+    }
+
+    top_stack_value = list_first_entry(&stack, struct stack_value, stack); 
+    value = top_stack_value->value;
+    list_del(&top_stack_value->stack);
+    kfree(top_stack_value);
+
+    return value;
 }
