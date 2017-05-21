aa66d81e0d6b4248852712d0a36968feb9901ff6
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2017-05-20 22:05:44 -0300

    Fix checkpatch.pl issues

--- sys_stack/sys_stack.c
+++ sys_stack/sys_stack.c
@@ -6,36 +6,36 @@
 static LIST_HEAD(stack);
 
 struct stack_value {
-    int value;
-    struct list_head stack;
+	int value;
+	struct list_head stack;
 };
 
 asmlinkage long sys_stack_push(int value)
 {
-    struct stack_value* new_stack_value;
-    
-    new_stack_value = kmalloc(sizeof(*new_stack_value), GFP_KERNEL);
-    new_stack_value->value = value;
-    INIT_LIST_HEAD(&new_stack_value->stack);
+	struct stack_value *new_stack_value;
 
-    list_add(&new_stack_value->stack, &stack);
-    return 0;
+	new_stack_value = kmalloc(sizeof(*new_stack_value), GFP_KERNEL);
+	new_stack_value->value = value;
+	INIT_LIST_HEAD(&new_stack_value->stack);
+
+	list_add(&new_stack_value->stack, &stack);
+	return 0;
 }
 
 asmlinkage long sys_stack_pop(void)
 {
-    int value;
-    struct stack_value* top_stack_value;
+	int value;
+	struct stack_value *top_stack_value;
 
-    if (list_empty(&stack)) {
-        printk("EMPTY STACK\n");
-        return -1;
-    }
+	if (list_empty(&stack)) {
+		pr_debug("EMPTY STACK\n");
+		return -1;
+	}
 
-    top_stack_value = list_first_entry(&stack, struct stack_value, stack); 
-    value = top_stack_value->value;
-    list_del(&top_stack_value->stack);
-    kfree(top_stack_value);
+	top_stack_value = list_first_entry(&stack, struct stack_value, stack);
+	value = top_stack_value->value;
+	list_del(&top_stack_value->stack);
+	kfree(top_stack_value);
 
-    return value;
+	return value;
 }
