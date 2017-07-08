c75343b32ca84905d2089382243dff03ad0390d7
Tiago Koji Castro Shibata <tiago.shibata@gmail.com>
2017-07-08 20:18:40 -0300

    Block on read to blocking_device, wakeup when data available

--- sys_blocking_dev/sys_blocking_dev.c
+++ sys_blocking_dev/sys_blocking_dev.c
@@ -1,33 +1,65 @@
 #include <linux/fs.h>
 #include <linux/kernel.h>
+#include <linux/list.h>
 #include <linux/miscdevice.h>
 #include <linux/module.h>
+#include <linux/slab.h>
+
+static DECLARE_WAIT_QUEUE_HEAD(wq);
+static LIST_HEAD(data_queue);
+
+struct device_data {
+	struct list_head head;
+	char data;
+};
+
+static char get_data(void)
+{
+	struct device_data *entry;
+	char c;
+
+	entry = list_first_entry(&data_queue, struct device_data, head);
+	c = entry->data;
+	list_del(&entry->head);
+	kfree(entry);
+	return c;
+}
+
+static long put_data(char c)
+{
+	struct device_data *entry = kmalloc(sizeof(*entry), GFP_KERNEL);
+	if (!entry) {
+		pr_debug("Device queue data allocation failed\n");
+		return -1;
+	}
+	entry->data = c;
+	list_add_tail(&entry->head, &data_queue);
+	return 0;
+}
 
 static ssize_t blocking_dev_read(struct file *filp, char __user *buffer,
 	size_t length, loff_t *ppos)
 {
-	return simple_read_from_buffer(buffer, length, ppos, "a", 1);
+	if (wait_event_interruptible(wq, !list_empty(&data_queue)))
+		return -ERESTARTSYS;
+
+	// FIXME Race condition if multiple readers
+	char c = get_data();
+	return simple_read_from_buffer(buffer, length, ppos, &c, 1);
 }
 
-static ssize_t blocking_dev_write(struct file *filp, const char __user *buffer,
-	size_t length, loff_t *ppos)
+asmlinkage long sys_write_device(int data)
 {
-	char module_buffer;
-	int retval;
-
-	if (length != 1)
-		return -EINVAL;
-
-	retval = simple_write_to_buffer(&module_buffer, 1, ppos, buffer, 1);
-	if (!retval || module_buffer != 'a')
-		return -EINVAL;
-	return retval;
+	int rc = put_data(data);
+	if (rc)
+		return rc;
+	wake_up_interruptible(&wq);
+	return 0;
 }
 
 static const struct file_operations blocking_dev_fops = {
 	.owner = THIS_MODULE,
-	.read  = blocking_dev_read,
-	.write = blocking_dev_write
+	.read  = blocking_dev_read
 };
 
 static struct miscdevice id_misc_device = {
